acfafe25db454dbe32a00b85a61ef6706e02e0d9
KBOverride <kabirsingh102@hotmail.com>
2017-10-14 00:23:10 -0400

    Added localStorage in app.js for moreInfo.html
    Needed to use localStorage to store information from the api call so I
    don't have to keep calling api requests constantly. Updated design on
    moreInfo.html so that it contains title, poster, overview and backdrop
    image. I will add more features later.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,6 +25,7 @@ xhr.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
     var myArr = JSON.parse(this.responseText);
     displayNowPlaying(myArr, counter);
+    localData(myArr);
   }
 });
 
@@ -32,24 +33,45 @@ comingSoon.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
     var myArr = JSON.parse(this.responseText);
     displayComingSoon(myArr, counter);
+    //localStorage(myArr);
   }
 });
 
-// Movies in theatres for past 2 months and sorted by popularity by default request
-
+// Requests made here appropiately
 if (fname == "index.html" || fname == "now-playing-page2.html" || window.location.href == "https://kb-top-10-movies.firebaseapp.com/") {
 	xhr.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.lte=2017-10-12&primary_release_date.gte=2017-08-10&page=1");
 	comingSoon.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.gte=2017-10-12&page=1");
 	counter = 1;
+	xhr.send(data);
+	comingSoon.send(data);
+
 } else if (fname == "now-playing-page3.html" || fname == "now-playing-page4.html") {
 	xhr.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.lte=2017-10-12&primary_release_date.gte=2017-08-10&page=2");
-	comingSoon.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.gte=2017-10-12&page=1");
+	comingSoon.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.gte=2017-10-12&page=2");
 	counter = 21;
+	xhr.send(data);
+	comingSoon.send(data);
+
+} else if (fname == "moreInfo.html") {
+	displayMoreInfo();
+}
+
+/* Saving data to session Storage for moreInfo.html*/
+
+function localData(myArr){
+	var clickedButton = $(".mdl-button");
+
+	$(clickedButton).click(function () {
+		var j = (this.id).replace( /^\D+/g, '');
+		j = j - 1;
+		localStorage.movieTitle = myArr.results[j].title;
+		localStorage.movieOverview = myArr.results[j].overview;
+		localStorage.movieBackDrop = myArr.results[j].backdrop_path;
+		localStorage.moviePoster = myArr.results[j].poster_path;
+	});
 }
 
 
-xhr.send(data);
-comingSoon.send(data);
 
 function displayNowPlaying(myArr, counter) {
 
@@ -69,6 +91,15 @@ function displayComingSoon(myArr, counter) {
 		$(".comingSoonDesc" + counter).text(myArr.results[i].overview);
 		counter++;
 	}
+}
+
+function displayMoreInfo() {
+
+	var urlStr = "url(http://image.tmdb.org/t/p/w1920" + localStorage.movieBackDrop + ")";
+	$(".backdropImg").css("background", urlStr + " center top no-repeat");
+	$(".moreInfoTitle").text(localStorage.movieTitle);
+	$(".overviewInfo").text(localStorage.movieOverview);
+	$("#imgPoster").attr("src", "http://image.tmdb.org/t/p/w342/" + localStorage.moviePoster);
 }
 
 function fileName() {
