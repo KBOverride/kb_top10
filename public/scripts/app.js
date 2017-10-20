fbbaab806307f56f561df252b35b6a17485bc068
KBOverride <kabirsingh102@hotmail.com>
2017-10-19 23:15:39 -0400

    Added Release Date (index only for now)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,25 @@
 var data = "{}";
 var counter = 0;
 
+// Getting current date
+var n = new Date();
+var year = n.getFullYear();
+var month = n.getMonth() + 1;
+var day = n.getDate();
+
+var monthPast = month - 2;
+var yearPast = year;
+var dayPast = day;
+if (monthPast < 0) {
+	monthPast = 12 + monthPast;
+	yearPast = yearPast - 1;
+}
+
+
 var fname = fileName();
 var nowPlaying = new XMLHttpRequest();
 var comingSoon = new XMLHttpRequest();
+var videos = new XMLHttpRequest();
 
 nowPlaying.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
@@ -27,15 +43,16 @@ comingSoon.addEventListener("readystatechange", function () {
 
 // Requests made here appropiately
 if (fname == "index.html" || fname == "now-playing-page2.html" || window.location.href == "https://kb-top-10-movies.firebaseapp.com/") {
-	nowPlaying.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.lte=2017-10-12&primary_release_date.gte=2017-08-10&page=1");
-	comingSoon.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.gte=2017-10-12&page=1");
+	nowPlaying.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.lte=" + year + "-" + month + "-" + day + "&primary_release_date.gte="
+	+ yearPast + "-" + monthPast + "-" + dayPast + "&page=1");
+	comingSoon.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.gte="  + year + "-" + month + "-" + day + "&page=1");
 	counter = 1;
 	nowPlaying.send(data);
 	comingSoon.send(data);
 
 } else if (fname == "now-playing-page3.html" || fname == "now-playing-page4.html") {
-	nowPlaying.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.lte=2017-10-12&primary_release_date.gte=2017-08-10&page=2");
-	comingSoon.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.gte=2017-10-12&page=2");
+	nowPlaying.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.lte=2017-10-19&primary_release_date.gte=2017-08-19&page=2");
+	comingSoon.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=4a302fed57f688d39421fdd5fc669830&language=en-US&primary_release_date.gte=2017-10-19&page=2");
 	counter = 21;
 	nowPlaying.send(data);
 	comingSoon.send(data);
@@ -56,6 +73,8 @@ function localData(myArr, flag){
 				localStorage.movieOverview = myArr.results[j].overview;
 				localStorage.movieBackDrop = myArr.results[j].backdrop_path;
 				localStorage.moviePoster = myArr.results[j].poster_path;
+				localStorage.movieReleaseDate = myArr.results[j].release_date;
+				localStorage.movieID = myArr.results[j].id;
     		}
 
     		if ($("#scroll-tab-2").hasClass("is-active") && (flag == 1)) {
@@ -63,6 +82,8 @@ function localData(myArr, flag){
 				localStorage.movieOverview = myArr.results[j].overview;
 				localStorage.movieBackDrop = myArr.results[j].backdrop_path;
 				localStorage.moviePoster = myArr.results[j].poster_path;
+				localStorage.movieReleaseDate = myArr.results[j].release_date;
+				localStorage.movieID = myArr.results[j].id;
     		}
 		});
 }
@@ -73,6 +94,7 @@ function displayNowPlaying(myArr, counter) {
 		$(".poster" + counter).attr("src", "http://image.tmdb.org/t/p/w185/" + myArr.results[i].poster_path);
 		$(".movieTitle" + counter).text(myArr.results[i].title);
 		$(".movieDesc" + counter).text(myArr.results[i].overview);
+		$(".releaseDateNow" + counter).text(myArr.results[i].release_date);
 		counter++;
 	}
 }
@@ -88,7 +110,6 @@ function displayComingSoon(myArr, counter) {
 }
 
 function displayMoreInfo() {
-
 	
 	var urlStr = "url(http://image.tmdb.org/t/p/w1920" + localStorage.movieBackDrop + ")";
 	$(".backdropImg").css("background", urlStr + " center top no-repeat");
@@ -104,3 +125,6 @@ function fileName() {
 	return page;
 }
 
+function getTrailers() {
+	var urlStr = "https://www.youtube.com/watch?v=";
+}
